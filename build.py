b208732efd2aaf5825161f433472d55c6680cf22
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2019-03-14 21:28:01 +0300

    Add missing build.py

---
+++ build.py
@@ -0,0 +1,48 @@
+# TCMalloc Conan package
+# Dmitriy Vetutnev, Odant, 2019
+
+
+import platform, os
+from copy import deepcopy
+from conan.packager import ConanMultiPackager
+
+
+# Common settings
+username = "odant" if "CONAN_USERNAME" not in os.environ else None
+# Windows settings
+visual_versions = ["14", "15"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
+visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
+dll_sign = False if "CONAN_DISABLE_DLL_SIGN" in os.environ else True
+
+
+def add_dll_sign(builds):
+    result = []
+    for settings, options, env_vars, build_requires, reference in builds:
+        options = deepcopy(options)
+        options["freeimage:dll_sign"] = dll_sign
+        result.append([settings, options, env_vars, build_requires, reference])
+    return result
+
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(
+        username=username,
+        visual_versions=visual_versions,
+        visual_runtimes=visual_runtimes,
+        exclude_vcvars_precommand=True
+    )
+    builder.add_common_builds(pure_c=True)
+    # Adjusting build configurations
+    builds = builder.items
+    if platform.system() == "Windows":
+        builds = add_dll_sign(builds)
+    # Replace build configurations
+    builder.items = []
+    for settings, options, env_vars, build_requires, _ in builds:
+        builder.add(
+            settings=settings,
+            options=options,
+            env_vars=env_vars,
+            build_requires=build_requires
+        )
+    builder.run()
