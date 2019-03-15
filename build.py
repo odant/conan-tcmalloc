2b7d9d81e777673c64f3234e91a9edc33264cfd0
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2019-03-15 13:10:11 +0300

    filter libcxx

--- build.py
+++ build.py
@@ -15,6 +15,14 @@ visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ els
 dll_sign = False if "CONAN_DISABLE_DLL_SIGN" in os.environ else True
 
 
+def filter_libcxx(builds):
+    result = []
+    for settings, options, env_vars, build_requires, reference in builds:
+        if settings["compiler.libcxx"] == "libstdc++11":
+            result.append([settings, options, env_vars, build_requires, reference])
+    return result
+
+
 def add_dll_sign(builds):
     result = []
     for settings, options, env_vars, build_requires, reference in builds:
@@ -31,11 +39,13 @@ if __name__ == "__main__":
         visual_runtimes=visual_runtimes,
         exclude_vcvars_precommand=True
     )
-    builder.add_common_builds(pure_c=True)
+    builder.add_common_builds(pure_c=False)
     # Adjusting build configurations
     builds = builder.items
     if platform.system() == "Windows":
         builds = add_dll_sign(builds)
+    if platform.system() == "Linux":
+        builds = filter_libcxx(builds)
     # Replace build configurations
     builder.items = []
     for settings, options, env_vars, build_requires, _ in builds:
