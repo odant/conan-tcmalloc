57f98cf1e6ac5d820d97d86de940648ba1cf2216
Arkady Yudintsev <udincev@odant.ru>
2024-04-10 11:26:13 +0300

    Version 2.15.0.1

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ def generateVersionH(version, current_time=datetime.now()):
 
 class TCMallocConan(ConanFile):
     name = "tcmalloc"
-    version = "2.15.0.0"
+    version = "2.15.0.1"
     license = "BSD 3-Clause"
     description = "Thread-Cached Malloc"
     url = "https://github.com/odant/conan-tcmalloc"
